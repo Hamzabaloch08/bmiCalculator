cb2c8f60e16dfd27facf67cadbac946ec88fc2ae
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-14 06:22:05 +0500

    create a bmi calculator

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', (e) => {
     const weight = parseInt(document.querySelector('#weight').value);
     const result = document.querySelector('#results');
 
-    if ( height === "" || height < 0 || isNaN(height)) {
+    if (height === "" || height < 0 || isNaN(height)) {
         result.innerHTML = "Please enter a valid height";
     } else if (weight === "" || weight < 0 || isNaN(weight)) {
         result.innerHTML = "Please enter a valid weight";
