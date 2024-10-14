8b0e3889435b60fda7f51d936ad7e502a714bebd
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-14 06:19:56 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const result = document.querySelector('#results');
+
+
+});
