3fa8dfa483f779c4cfb8530347eef6b1852fb21a
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-14 06:20:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -11,5 +11,18 @@ form.addEventListener('submit', (e) => {
         result.innerHTML = "Please enter a valid height";
     } else if (weight === "" || weight < 0 || isNaN(weight)) {
         result.innerHTML = "Please enter a valid weight";
+    } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        result.innerHTML = `<span>${bmi}</span>`;
+
+        if (bmi <= 18.6) {
+            result.innerHTML += `<p>You are underweight</p>`;
+        } else if (bmi > 18.6 && bmi <= 24.9) {
+            result.innerHTML += `<p>You are normal weight</p>`;
+        } else if (bmi > 24.9 && bmi <= 29.9) {
+            result.innerHTML += `<p>You are overweight</p>`;
+        } else {
+            result.innerHTML += `<p>You are obese</p>`;
+        }
     }
 });
